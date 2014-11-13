a20115fdf67d8d1acdf86da18ebc13868577ffad
Jameson Knoll <jmknoll@Xiaoyi-Huangs-iMac.local>
2014-11-13 14:21:43 +0800

    basic layout and routing

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
+  root 'reports#index'
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -13,6 +15,7 @@ Rails.application.routes.draw do
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
+  resources :reports
 
   # Example resource route with options:
   #   resources :products do
