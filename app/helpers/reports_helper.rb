c1c2260bae4df86cbfe82c632d0200e6650ca5b2
Jameson Knoll <jmknoll@Xiaoyi-Huangs-iMac.local>
2014-11-14 01:19:59 +0800

    build out personalized recommendations

--- app/helpers/reports_helper.rb
+++ app/helpers/reports_helper.rb
@@ -1,2 +1,33 @@
 module ReportsHelper
+	
+	def reading_personal
+		if @report.scale_reading > 699
+			render "shared/reading_high"
+		elsif @report.scale_reading  > 499
+			render "shared/reading_medium"
+		else 
+			render "shared/reading_low"
+		end
+	end
+
+	def math_personal
+		if @report.scale_math > 699
+			render "shared/math_high"
+		elsif @report.scale_math  > 499
+			render "shared/math_medium"
+		else 
+			render "shared/math_low"
+		end
+	end
+
+	def writing_personal
+		if @report.scale_writing > 699
+			render "shared/writing_high"
+		elsif @report.scale_writing  > 499
+			render "shared/writing_medium"
+		else 
+			render "shared/writing_low"
+		end
+	end
+
 end
